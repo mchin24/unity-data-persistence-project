1137f518787b126398d504c07a7671c0398b78a5
Mike Chin <mchin24@gmail.com>
2025-11-17 13:01:31 -0500

    Setting player name is functional - If there's no player name set at startup, the player name field is   displayed  - Player name is set by pressing "Set" button or hitting "Enter" key

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager Instance;
+    public string currentPlayerName = "";
+    public int highScore;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,14 +1,45 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class MenuUIHandler : MonoBehaviour
 {
-    public GameObject PlayerNameContainer;
-    public GameObject StartContainer;
+    public GameObject playerNameContainer;
+    public GameObject menuContainer;
+
+    public GameObject playerNameField;
+    public GameObject currentPlayerName;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        PlayerNameContainer.SetActive(true);
-        StartContainer.SetActive(false);
+        if (String.IsNullOrEmpty(DataManager.Instance.currentPlayerName))
+        {
+            DisplayNameField();
+        }
+        else
+        {
+            DisplayGameMenu();
+        }
+    }
+
+    public void DisplayNameField()
+    {
+        playerNameField.GetComponent<TMP_InputField>().text = DataManager.Instance.currentPlayerName;
+        playerNameContainer.SetActive(true);
+        menuContainer.SetActive(false);
+    }
+
+    public void DisplayGameMenu()
+    {
+        currentPlayerName.GetComponent<TMP_Text>().text = "Player: " + DataManager.Instance.currentPlayerName;
+        playerNameContainer.SetActive(false);
+        menuContainer.SetActive(true);
+    }
+
+    public void SetPlayerName()
+    {
+        DataManager.Instance.currentPlayerName = playerNameField.GetComponent<TMP_InputField>().text;
+        DisplayGameMenu();
     }
 }
