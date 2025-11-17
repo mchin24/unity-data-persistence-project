4d0978572c0c2f64049d8729c5c05ca810cde33b
Mike Chin <mchin24@gmail.com>
2025-11-17 14:29:21 -0500

    Added player name to main scene - Name is loaded from DataManager upon scene start

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,6 +11,7 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text NameText;
     public GameObject GameOverText;
     
     private bool m_Started = false;
@@ -22,6 +23,11 @@ public class MainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (DataManager.Instance != null)
+        {
+            NameText.text = "Player : " + DataManager.Instance.currentPlayerName;
+        }
+        
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
